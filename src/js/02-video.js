92a9c2af5f44946368c5115a5232d30f2b4e44a2
Oleksandra-lab <tka4olex@gmail.com>
2023-07-30 21:02:24 +0300

    finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,14 @@
 import Player from '@vimeo/player';
-
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
+const currentTime = localStorage.getItem(STORAGE_KEY) || 0;
 
-const onPlay = function(data) {
-    // data is an object containing properties specific to that event
+const onTimeUpdate = function({seconds}) {
+ 
+    localStorage.setItem(STORAGE_KEY, seconds)
 };
 
-player.on('play', onPlay);
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+player.setCurrentTime(currentTime)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,26 +1,40 @@
- const form = document.querySelector('.feedback-form')
-form.addEventListener('submit', handleForm)
-form.reset()
-const array = JSON.parse(localStorage.getItem("feedback-form-state")) || [];
+ import throttle from "lodash.throttle";
+ const formEl = document.querySelector('.feedback-form')
+formEl.addEventListener('submit', handleForm)
+formEl.addEventListener('input', throttle(onInput, 500))
+const STORAGE_KEY = "feedback-form-state";
+let formState = {};
+
+
 
 function handleForm(evt){
     evt.preventDefault()
-
-    const email = evt.target.elements.email.value;
-    const message = evt.target.elements.message.value;
-
-
-    const object = {
-        email,
-        message,
-    }
-
-    array.push(object);
-
-    localStorage.setItem("feedback-form-state", JSON.stringify(array))
-
-    console.log(array);
-
-    evt.currentTarget.reset()
-    
+    console.log(formState);
+    formState = {}
+    localStorage.removeItem(STORAGE_KEY)
+    evt.currentTarget.reset()    
 }
+
+function onInput (evt) {
+formState[evt.target.name]=evt.target.value.trim();
+localStorage.setItem(STORAGE_KEY, JSON.stringify(formState));
+}
+
+function onLoad(){
+    try {
+        const data = localStorage.getItem(STORAGE_KEY)
+    if(!data)   return; 
+    formState = JSON.parse(data);
+    Object.entries(formState).forEach(([key, val]) =>{
+        formEl.elements[key].value = val;
+    } )
+        
+    } catch (error) {
+        console.log(error.message);
+        
+    }
+    
+
+
+}
+window.addEventListener('load', onLoad)
